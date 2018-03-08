09540e65d5b4915638adb795576a4aa8e23143f8
StanTheSoupKing <stanthesoupking@gmail.com>
2018-03-08 13:17:35 +1100

    Setting up JavaFX

---
+++ MapReduceFX.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * MapReduceFX:
+ * Simple program for performing the map reducealgorithm
+ * on a body of text.
+ * 
+ * @author Stanley Fuller
+ */
+public class MapReduceFX extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+}
